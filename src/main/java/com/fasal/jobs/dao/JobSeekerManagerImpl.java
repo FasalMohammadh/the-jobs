f797a467fb87036cb6a1169d63a2466f18a109ba
fasalmohammadh <fazalmohammed9367@gmail.com>
2023-09-06 03:10:47 +0530

    refactored and covert queries to Stored Procedures

--- src/main/java/com/fasal/jobs/dao/JobSeekerManagerImpl.java
+++ src/main/java/com/fasal/jobs/dao/JobSeekerManagerImpl.java
@@ -17,7 +17,7 @@ public class JobSeekerManagerImpl implements JobSeekerManager {
   @Override
   public boolean create(JobSeeker jobSeeker) throws ClassNotFoundException, SQLException {
     Connection connection = new DatabaseFactory().getDatabase(DatabaseType.MYSQL).getConnection();
-    String query = "INSERT INTO job_seeker (id,first_name,last_name,email,phone_number)  VALUES (?,?,?,?,?);";
+    String query = "call CreateJobSeeker(?,?,?,?,?)";
     PreparedStatement createStatement = connection.prepareStatement(query);
 
     createStatement.setString(1, jobSeeker.getId());
@@ -36,7 +36,7 @@ public class JobSeekerManagerImpl implements JobSeekerManager {
   @Override
   public boolean update(JobSeeker jobSeeker) throws ClassNotFoundException, SQLException {
     Connection connection = new DatabaseFactory().getDatabase(DatabaseType.MYSQL).getConnection();
-    String query = "UPDATE job_seeker SET first_name=?,last_name=?,email=?,phone_number=? WHERE id=?";
+    String query = "call UpdateJobSeeker(?,?,?,?,?)";
     PreparedStatement updateStatement = connection.prepareStatement(query);
 
     updateStatement.setString(1, jobSeeker.getFirstName());
@@ -55,7 +55,7 @@ public class JobSeekerManagerImpl implements JobSeekerManager {
   @Override
   public boolean delete(String jobSeekerId) throws ClassNotFoundException, SQLException {
     Connection connection = new DatabaseFactory().getDatabase(DatabaseType.MYSQL).getConnection();
-    String query = "Delete FROM job_seeker WHERE id=?";
+    String query = "call DeleteJobSeeker(?)";
     PreparedStatement deleteStatement = connection.prepareStatement(query);
 
     deleteStatement.setString(1, jobSeekerId);
@@ -70,7 +70,7 @@ public class JobSeekerManagerImpl implements JobSeekerManager {
   @Override
   public JobSeeker findUnique(String jobSeekerId) throws ClassNotFoundException, SQLException {
     Connection connection = new DatabaseFactory().getDatabase(DatabaseType.MYSQL).getConnection();
-    String query = "SELECT * FROM job_seeker WHERE id=?";
+    String query = "call FindJobSeeker(?)";
     PreparedStatement findStatement = connection.prepareStatement(query);
 
     findStatement.setString(1, jobSeekerId);
@@ -78,8 +78,7 @@ public class JobSeekerManagerImpl implements JobSeekerManager {
 
     JobSeeker jobSeeker = null;
     if (result.next())
-      jobSeeker = new JobSeeker(result.getString("id"), result.getString("email"), result.getString("phoneNumber"),
-              result.getString("first_name"), result.getString("last_name"), result.getString("created_at"));
+      jobSeeker = getJobSeekerFromResult(result);
 
     result.close();
     findStatement.close();
@@ -91,16 +90,13 @@ public class JobSeekerManagerImpl implements JobSeekerManager {
   @Override
   public List<JobSeeker> findMany() throws ClassNotFoundException, SQLException {
     Connection connection = new DatabaseFactory().getDatabase(DatabaseType.MYSQL).getConnection();
-    String query = "SELECT * FROM job_seeker";
+    String query = "call FindJobSeekers()";
     Statement findStatement = connection.createStatement();
 
     ResultSet result = findStatement.executeQuery(query);
     List<JobSeeker> jobSeekerList = new ArrayList<>();
     while (result.next()) {
-      JobSeeker jobSeeker = new JobSeeker(result.getString("id"), result.getString("email"),
-              result.getString("phone_number"),
-              result.getString("first_name"), result.getString("last_name"), result.getString("created_at"));
-
+      JobSeeker jobSeeker = getJobSeekerFromResult(result);
       jobSeekerList.add(jobSeeker);
     }
 
@@ -110,4 +106,19 @@ public class JobSeekerManagerImpl implements JobSeekerManager {
 
     return jobSeekerList;
   }
+
+  private JobSeeker getJobSeekerFromResult(ResultSet result) throws SQLException {
+    JobSeeker jobSeeker = new JobSeeker(
+            result.getString("id"),
+            result.getString("email"),
+            result.getString("phone_number"),
+            result.getString("first_name"),
+            result.getString("last_name"),
+            result.getString("created_at")
+    );
+
+    return jobSeeker;
+  }
+
+
 }
